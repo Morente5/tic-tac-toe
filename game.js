ef9c49233fcb9eb32c9e186fed06b93af8d1c205
Alejandro Morente <morente5@hotmail.es>
2016-10-24 23:49:02 +0200

    Started winning detection and info messages

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ class View {
 	constructor(controller) {
 		this.controller = controller;
 		this.board = document.getElementById("board");
+		this.message = document.getElementById("message");
 	}
 
 	getCell(row, col) {
@@ -14,6 +15,10 @@ class View {
 		this.getCell(row, col).innerHTML = player.stone;
 		this.getCell(row, col).className = player.stone;
 	}
+
+	setMessage(message) {
+		this.message.innerHTML = message;
+	}
 }
 
 
@@ -23,27 +28,32 @@ class TicTacToe {
 	constructor() {
 		this.players = [new Player(this, "X"), new Player(this, "O")].shuffle();
 		this.board = [[null, null, null], [null, null, null], [null, null, null]];
-		this.winningCombinations = [
-			// rows
-			[this.board[0][0], this.board[0][1], this.board[0][2]],
-			[this.board[1][0], this.board[1][1], this.board[1][2]],
-			[this.board[2][0], this.board[2][1], this.board[2][2]],
-			// columns
-			[this.board[0][0], this.board[1][0], this.board[2][0]],
-			[this.board[0][1], this.board[1][1], this.board[2][1]],
-			[this.board[0][2], this.board[1][2], this.board[2][2]],
-			// diagonal
-			[this.board[0][0], this.board[1][1], this.board[2][2]],
-			[this.board[0][2], this.board[1][1], this.board[2][1]],
-			];
+		this.winner = null;
+	}
+
+	get winningCombinations() { 
+		return [
+		// rows
+		[this.board[0][0], this.board[0][1], this.board[0][2]],
+		[this.board[1][0], this.board[1][1], this.board[1][2]],
+		[this.board[2][0], this.board[2][1], this.board[2][2]],
+		// columns
+		[this.board[0][0], this.board[1][0], this.board[2][0]],
+		[this.board[0][1], this.board[1][1], this.board[2][1]],
+		[this.board[0][2], this.board[1][2], this.board[2][2]],
+		// diagonal
+		[this.board[0][0], this.board[1][1], this.board[2][2]],
+		[this.board[0][2], this.board[1][1], this.board[2][1]],
+		];
 	}
 
 	move(row, col) {
-		if (!this.board[row][col]) {
-			this.players[0].move(row, col);
-			this.players[0].isWinner();
+		var occupied = this.board[row][col];
+		if (!occupied) {
+			var move = this.players[0].move(row, col);
+			var winn = this.players[0].win();
 			this.players.reverse();
-			return true;
+			return {movement: move, winner: winn};
 		}
 	}
 
@@ -59,6 +69,16 @@ class Player {
 		this.game.board[row][col] = this;
 	}
 
+	win() {
+		if (this.game.winningCombinations.some(elem => elem.every(elem2 => elem2 == this))) {
+			this.game.winner = this;
+			return this;
+		}
+		else {
+			return null;
+		}
+	}
+
 }
 
 
@@ -69,23 +89,31 @@ class Controller {
 		this.game = new TicTacToe();
 		this.view = new View(this);
 		this.generateEvents();
+		this.view.setMessage(this.game.players[0].stone + " turn");
 	}
 
 	generateEvents() {
 		for (let row = 0; row < 3; row++) {
 			for (let col = 0; col < 3; col++) {
-				this.view.getCell(row, col).addEventListener("click", () => this.move(row, col));
+				this.view.getCell(row, col).addEventListener("click", () => this.click(row, col));
 			}
 		}
 	}
 
-	move(row, col) {
-		var player = this.game.players[0];
-		// If movement was successful, set stone
-		if (this.game.move(row, col)) {
-			this.view.setStone(player, row, col);
+	click(row, col) {
+		// Only if there is no winner
+		if (!this.game.winner) {
+			var player = this.game.players[0];
+			var result = this.game.move(row, col);
+			// If movement was successful, set stone and message
+			if (result.movement) {
+				this.view.setStone(player, row, col);
+				this.view.setMessage(this.game.players[0].stone + " turn");
+			}
+			if (result.winner) {
+				this.view.setMessage(player.stone + " won");
+			}
 		}
-
 	}
 
 }
